8148e5b6455869f977c967fc3db2ffc8ca15f5b6
lizhongyi3 <1198721761@qq.com>
2023-05-23 23:18:26 +0800

    Math&Random类使用

--- src/demo/study/javaApi/BaoZhuangDemo.java
+++ src/demo/study/javaApi/BaoZhuangDemo.java
@@ -1,2 +1,25 @@
-package demo.study.javaApi;public class BaoZhuangDemo {
+package demo.study.javaApi;
+
+public class BaoZhuangDemo {
+    public static void main(String[] args) {
+        int a = 20;
+        Integer in = new Integer(a);
+        System.out.println(in.intValue());
+        System.out.println(args.toString());
+        int w = Integer.parseInt(args[0]);
+        int h = Integer.parseInt(args[1]);
+
+        System.out.println("宽为：" + w);
+        System.out.println("长为：" + h);
+
+        for (int i = 0; i < h; i ++){
+            StringBuffer sb = new StringBuffer();
+            for(int j = 0; j < w; j++){
+                sb.append("*");
+            }
+            System.out.println(sb);
+        }
+
+
+    }
 }
--- src/demo/study/javaApi/DateDemo.java
+++ src/demo/study/javaApi/DateDemo.java
@@ -1,2 +1,24 @@
-package demo.study.javaApi;public class DateDemo {
+package demo.study.javaApi;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+
+        Date date1 = new Date();
+        System.out.println(date1);
+
+        Date date2 = new Date(System.currentTimeMillis());
+        System.out.println(date2);
+
+        Date date3 = new Date(11111111);
+        System.out.println(date3);
+
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+
+
+    }
 }
